397718da62943240e96201080f9d20796c33899f
cbonner26118 <cbonner26118@gmail.com>
2021-08-23 18:57:25 -0700

    Added Nodmailer to API, sends email, but hangs up on final fetch. Must refactor.

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,1 +1,33 @@
-/* Going to build the API route for sending contact emails */
+require('dotenv').config();
+
+export default function (req, res) {
+	let nodemailer = require('nodemailer');
+	const email = process.env.NODE_EMAIL;
+	const password = process.env.NODE_EMAIL_PASSWORD;
+	const contactEmail = process.env.CONTACT_EMAIL;
+
+	const transporter = nodemailer.createTransport({
+		port: 465,
+		host: 'smtp.gmail.com',
+		auth: {
+			user: email,
+			pass: password,
+		},
+		secure: true,
+	});
+
+	const mailData = {
+		from: email,
+		to: contactEmail,
+		subject: req.body.subject,
+		text: req.body.message + ' | Sent from: ' + req.body.email + req.body.phone,
+		html: `<div>${req.body.message}</div><p>Sent from: ${req.body.email} + '|' ${req.body.phone}`,
+	};
+
+	transporter.sendMail(mailData, function (err, info) {
+		if (err) console.log(err);
+		else console.log(info);
+	});
+
+	res.status(200);
+}
